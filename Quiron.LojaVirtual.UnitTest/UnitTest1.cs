c274a39ae2cc7b600d52c174a9ec686fa725ebde
rodrigooliveira94 <rodrigooliveirafarias@yahoo.com.br>
2015-07-19 21:05:38 -0300

    Teste Paginação

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.Controllers.HtmlHelpers;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -10,5 +13,37 @@ namespace Quiron.LojaVirtual.UnitTest
         public void TestMethod1()
         {
         }
+
+        [TestMethod]
+
+        public void TestarSeAPaginacaoEstaSendoGeradaCorretamente()
+        {
+            //Arrange
+            HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 10,
+                ItensTotal = 28
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            //Act
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            //Assert
+
+            Assert.AreEqual(
+                @"<a class=""btn btn-default"" href=""Pagina1"" >1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"" >2</a>"
+                + @"<a class=""btn btn-default"" href=""Pagina1"" >3</a>", resultado.ToString()
+            );
+
+        }
+
+
+
     }
 }
